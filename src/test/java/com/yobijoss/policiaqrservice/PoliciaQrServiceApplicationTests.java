af4e11f2aba1e9b233b1f75d8ca5281a62b89cdd
Jose Angel Garcia Salinas <joseangg@joseangg01-mac.local>
2019-04-29 17:42:27 -0500

    first commit

---
+++ src/test/java/com/yobijoss/policiaqrservice/PoliciaQrServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yobijoss.policiaqrservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PoliciaQrServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
